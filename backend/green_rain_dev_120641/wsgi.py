8a6712aed8cd9b416f5cc82ab42d05f01ec72a8e
Crowdbotics <engineering@crowdbotics.com>
2023-10-06 11:03:57 +0000

    Initial commit

---
+++ backend/green_rain_dev_120641/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for green_rain_dev_120641 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "green_rain_dev_120641.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "green-rain-dev-120641",
+  url: "https://green-rain-dev-120641.botics.co",
+  api: "https://green-rain-dev-120641.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
